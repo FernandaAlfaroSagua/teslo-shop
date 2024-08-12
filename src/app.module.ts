851ba2847620aa6fc25c60917382272be944b7d2
Fernanda Alfaro <fernanda.alfaro@borealis.cl>
2024-08-11 23:01:26 -0400

    Fin seccion 16

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,17 +3,26 @@ import { ConfigModule } from '@nestjs/config';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { join } from 'path';
+import { AuthModule } from './auth/auth.module';
 import { CommonModule } from './common/common.module';
 import { FilesModule } from './files/files.module';
+import { MessageWsModule } from './message-ws/message-ws.module';
 import { ProductsModule } from './products/products.module';
 import { SeedModule } from './seed/seed.module';
-import { AuthModule } from './auth/auth.module';
-import { MessageWsModule } from './message-ws/message-ws.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
+      ssl: process.env.STAGE === 'prod',
+      extra: {
+        ssl:
+          process.env.STAGE === 'prod'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      },
       type: 'postgres',
       host: process.env.DB_HOST,
       port: +process.env.DB_PORT,
